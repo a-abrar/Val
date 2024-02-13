d308245683065721b8bd62b3ab54dceab109cad7
Abrar <abrarakhunji0@gmail.com>
2024-02-13 13:30:56 +0530

    Initial Commit

---
+++ vale.js
@@ -0,0 +1,19 @@
+document.getElementById("noBtn").addEventListener("click", function() {
+    var bodyWidth = document.body.clientWidth;
+    var bodyHeight = document.body.clientHeight;
+    var buttonWidth = document.getElementById("noBtn").offsetWidth;
+    var buttonHeight = document.getElementById("noBtn").offsetHeight;
+    
+    var randomX = Math.random() * (bodyWidth - buttonWidth);
+    var randomY = Math.random() * (bodyHeight - buttonHeight);
+  
+    document.getElementById("noBtn").style.position = "absolute";
+    document.getElementById("noBtn").style.left = randomX + "px";
+    document.getElementById("noBtn").style.top = randomY + "px";
+  });
+  
+  
+  document.getElementById("yesBtn").addEventListener("click", function() {
+    window.location.href = "valentine.html";
+  });
+  
